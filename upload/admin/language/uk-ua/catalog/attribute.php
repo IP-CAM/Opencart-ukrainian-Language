e4144e8667e2537b0105681f5f6e828077d6fd69
Andrew <51118083+ArionThinker@users.noreply.github.com>
2023-09-13 11:19:55 +0300

    Update attribute.php

--- upload/admin/language/uk-ua/catalog/attribute.php
+++ upload/admin/language/uk-ua/catalog/attribute.php
@@ -1,26 +1,26 @@
 <?php
-// Heading
-$_['heading_title']          = 'Attributes';
+// Заголовок
+$_['heading_title']          = 'Атрибути';
 
-// Text
-$_['text_success']           = 'Success: You have modified attributes!';
-$_['text_list']              = 'Attribute List';
-$_['text_add']               = 'Add Attribute';
-$_['text_edit']              = 'Edit Attribute';
+// Текст
+$_['text_success']           = 'Успіх: Ви змінили атрибути!';
+$_['text_list']              = 'Список атрибутів';
+$_['text_add']               = 'Додати атрибут';
+$_['text_edit']              = 'Редагувати атрибут';
 
-// Column
-$_['column_name']            = 'Attribute Name';
-$_['column_attribute_group'] = 'Attribute Group';
-$_['column_sort_order']      = 'Sort Order';
-$_['column_action']          = 'Action';
+// Колонки
+$_['column_name']            = 'Назва атрибута';
+$_['column_attribute_group'] = 'Група атрибутів';
+$_['column_sort_order']      = 'Порядок сортування';
+$_['column_action']          = 'Дія';
 
-// Entry
-$_['entry_name']             = 'Attribute Name';
-$_['entry_attribute_group']  = 'Attribute Group';
-$_['entry_sort_order']       = 'Sort Order';
+// Введення
+$_['entry_name']             = 'Назва атрибута';
+$_['entry_attribute_group']  = 'Група атрибутів';
+$_['entry_sort_order']       = 'Порядок сортування';
 
-// Error
-$_['error_permission']       = 'Warning: You do not have permission to modify attributes!';
-$_['error_attribute_group']  = 'Attribute Group Required!';
-$_['error_name']             = 'Attribute Name must be between 1 and 64 characters!';
-$_['error_product']          = 'Warning: This attribute cannot be deleted as it is currently assigned to %s products!';
+// Помилки
+$_['error_permission']       = 'Попередження: У вас немає дозволу на зміну атрибутів!';
+$_['error_attribute_group']  = 'Необхідна група атрибутів!';
+$_['error_name']             = 'Назва атрибута повинна бути від 1 до 64 символів!';
+$_['error_product']          = 'Попередження: Цей атрибут не може бути видалений, оскільки він наразі призначений для %s товарів!';
